1d925e22feb7829873a41681c8428a59efbe07fc
Mitchell Loeppky <mrl@amazon.com>
2019-07-09 12:29:25 -0700

    Add MPEG-DASH playback, LIVE_REPLAY playback mode, and other UI enhancements.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## Amazon Kinesis Video Streams HLS Viewer
+## Amazon Kinesis Video Streams Media Viewer
 
-Easily view HLS output of Amazon Kinesis Video Streams
+Easily view HLS and MPEG-DASH output of Amazon Kinesis Video Streams
 
 * * *
 ## About
-This is a simple static webpage that simplifies testing and experiments with HLS output from Amazon Kinesis Video Streams. This is based on the <a href="https://docs.aws.amazon.com/kinesisvideostreams/latest/dg/how-hls.html">documentation</a>.
+This is a simple static webpage that simplifies testing and experiments with HLS and MPEG-DASH output from Amazon Kinesis Video Streams. This is based on the <a href="https://docs.aws.amazon.com/kinesisvideostreams/latest/dg/how-hls.html">documentation</a>.
 
 ## Getting Started
 The easiest and recommended approach is to use the Github-hosted version of this project. The second method is to clone/download this project and host it in your own S3 bucket or even locally.
@@ -12,10 +12,10 @@ The easiest and recommended approach is to use the Github-hosted version of this
 ### Using the Github-hosted UI
 Because the project is a collection of static HTML and JavaScript, you can use the application without deploying the code into your own AWS account.
 
- To get started, go to the hosted [Kinesis Video Streams HLS Viewer](https://aws-samples.github.io/amazon-kinesis-video-streams-hls-viewer/).
+ To get started, go to the hosted [Kinesis Video Streams Media Viewer](https://aws-samples.github.io/amazon-kinesis-video-streams-media-viewer/).
 
 <br />
-<a href="https://aws-samples.github.io/amazon-kinesis-video-streams-hls-viewer/"><img border="0" src="sample_app.png"></a>
+<a href="https://aws-samples.github.io/amazon-kinesis-video-streams-media-viewer/"><img border="0" src="sample_app.png"></a>
 
 ## License Summary
 
---
+++ ui.js
@@ -0,0 +1,99 @@
+configureLogging();
+
+// Update all of the corresonding fields when switching protocol or playback mode
+$('#protocol').change(function() {
+    updateFieldsCorrespondingToStreamingProtocol();
+});
+$('#playbackMode').change(function() {
+    updateFieldsCorrespondingToPlaybackMode();
+});
+
+// Read/Write all of the fields to/from localStorage so that fields are not lost on refresh.
+[
+    'protocol',
+    'player',
+    'region',
+    'accessKeyId',
+    'secretAccessKey',
+    'sessionToken',
+    'endpoint',
+    'streamName',
+    'playbackMode',
+    'discontinuityMode',
+    'displayFragmentTimestamp',
+    'displayFragmentNumber',
+    'startTimestamp',
+    'endTimestamp',
+    'fragmentSelectorType',
+    'maxResults',
+    'expires'
+].forEach(function(field) {
+    var id = "#" + field;
+
+    // Read field from localStorage
+    try {
+        var localStorageValue = localStorage.getItem(field);
+        if (localStorageValue) { 
+            $(id).val(localStorageValue);
+            $(id).trigger('change');
+        }
+    } catch (e) { /* Don't use localStorage */ }
+
+    // Write field to localstorage on change event
+    $(id).change(function() {
+        try {
+            localStorage.setItem(field, $(id).val());
+        } catch (e) { /* Don't use localStorage */ }
+    });
+});
+
+// Initially hide the player elements
+$('.player').hide();
+
+function configureLogging() {
+    console._error = console.error;
+    console.error = function(messages) {
+        log('ERROR', Array.prototype.slice.call(arguments));
+        console._error.apply(this, arguments);
+    }
+
+    console._log = console.log;
+    console.log = function(messages) {
+        log('INFO', Array.prototype.slice.call(arguments));
+        console._log.apply(this, arguments);
+    }
+
+    function log(level, messages) {
+        var text = '';
+        for (message of messages) {
+            if (typeof message === 'object') { message = JSON.stringify(message, null, 2); }
+            text += ' ' + message;
+        }
+        $('#logs').append($('<div>').text('[' + level + ']' + text + '\n'));
+    }
+}
+
+function updateFieldsCorrespondingToStreamingProtocol() {
+    var isDASH = $('#protocol').val() === 'DASH';
+    $('#containerFormat').prop('disabled', isDASH);
+    $('#discontinuityMode').prop('disabled', isDASH);  
+    $('#displayFragmentNumber').prop('disabled', !isDASH); 
+
+    var players = isDASH ? DASH_PLAYERS : HLS_PLAYERS;
+    $('#player').empty();
+    players.forEach(function (player) {
+        var option = $('<option>').text(player);
+        $('#player').append(option);
+    });
+    $('#player').trigger('change');  
+}
+
+function updateFieldsCorrespondingToPlaybackMode() {
+    var isLive = $('#playbackMode').val() === 'LIVE';
+    $('#startTimestamp').prop('disabled', isLive);
+    $('#endTimestamp').prop('disabled', isLive);
+}
+
+$('.loader').hide();
+$('.main').show();
+console.log("Page loaded")
